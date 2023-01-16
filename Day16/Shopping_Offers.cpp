ee0c1926f445c76e8bc9237413dacd9b2c60c825
Sahil Dubey <dubeysahil1998@gmail.com>
2023-01-16 20:16:22 +0530

    Day 16 practise problems added

---
+++ Day16/Shopping_Offers.cpp
@@ -0,0 +1,48 @@
+// 638. Shopping Offers
+
+#include  <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    map<vector<int>,int>state;
+    int noOffers(vector<int>&price, vector<int>&needs)
+    {
+        int totalPrice = 0;
+        for(int i = 0; i < price.size(); i++)
+            totalPrice += price[i] * needs[i];
+        return totalPrice;
+    }
+    void subtractOffer(vector<int>&offer, vector<int>&needs)
+    {
+        for(int i = 0; i < needs.size(); i++)
+            needs[i] -= offer[i];
+    }
+    bool noNegativeNeed(vector<int>&needs)
+    {
+        for(auto need : needs) 
+            if(need < 0) 
+                return false;
+        return true;
+    }
+    void addOffer(vector<int>&offer, vector<int>&needs)
+    {
+        for(int i = 0; i < needs.size(); i++)
+            needs[i] += offer[i];
+    }
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+           if(state[needs]) return state[needs];
+           int bestPrice = noOffers(price, needs);     
+           for(auto offer : special)
+           {
+               subtractOffer(offer, needs);
+               if(noNegativeNeed(needs))
+               {
+                   int withOffer = offer.back() + shoppingOffers(price, special, needs);
+                   bestPrice = min(bestPrice , withOffer);
+               }
+               addOffer(offer, needs);
+           }
+           return state[needs] = bestPrice;
+    }
+};
